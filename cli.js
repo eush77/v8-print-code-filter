7d41a2c35dfbcff8926dea31e94adca085fbd34a
Eugene Sharygin <eush77@gmail.com>
2015-05-20 00:45:15 +0300

    Add implementation

--- cli.js
+++ cli.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
-var mapKeys = require('map-keys');
+var mapKeys = require('map-keys'),
+    codeDumpParser = require('v8-code-dump-parser'),
+    concat = require('parse-concat-stream');
 
 var argv = mapKeys(require('yargs')
                    .usage('Usage:  $0 [filter_expr]...')
@@ -10,4 +12,16 @@ var argv = mapKeys(require('yargs')
                      return key.replace(/-/g, '_');
                    });
 
-console.log(argv);
+
+process.stdin.pipe(concat({ parse: codeDumpParser }, function (err, sections) {
+  if (err) throw err;
+
+  sections = sections.filter(function (section) {
+    var code = section.code || section.optimizedCode;
+    return Object.keys(argv).some(function (key) {
+      return code[key] == argv[key];
+    });
+  });
+
+  process.stdout.write(codeDumpParser.stringify(sections));
+}));
